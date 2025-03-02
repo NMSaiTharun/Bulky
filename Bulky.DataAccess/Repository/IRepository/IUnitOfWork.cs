6094fbbc60858f0a615d88d6a5100d38231b3af0
NMSaiTharun <nmsaitarun@gmail.com>
2025-03-02 16:31:59 +0530

    adding-order details-2-3-2025

--- Bulky.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ Bulky.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -12,6 +12,8 @@ namespace BulkyBook.DataAccess.Repository.IRepository
         IProductRepository Product { get; }
         ICompanyRepository Company { get; }
         IShoppingCartRepository ShoppingCart { get; }
+        IOrderHeaderRepository OrderHeader { get; }
+        IOrderDetailRepository OrderDetail { get; }
         IApplicationUserRepository ApplicationUser { get; }
         void Save();
     }
